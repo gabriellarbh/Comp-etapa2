94dd241ba823f5607defb305a9995284ecbb7c5d
Thiago Bell <thiago@tim>
2016-09-04 23:12:42 -0300

    a

--- hash.h
+++ hash.h
@@ -1,7 +1,9 @@
 #define HASH_SIZE 997
 
+typedef HASHCELL;
+
 //returns -1 if hash not initialized
-int addHash(char* text, int token);
+HASHCELL* addHash(char* text, int token);
 //returns -1 if hash not initialized
 //and -2 if no record could be found
 int getHash(char* text);
